04503ff92db909a771c7d9a3a63287bc5e435064
alexjli <alexjli@mit.edu>
2020-06-15 18:04:40 -0500

    modifications for a full dummy training script

--- data.py
+++ data.py
@@ -3,30 +3,40 @@ import torch
 from torch.nn.utils.rnn import pad_sequence
 import pickle
 from scipy.linalg import block_diag
+import glob
 
 def convert(tensor):
     return torch.from_numpy(tensor)
 
 class Dataset():
-    def __init__(self, pdb_ids):
+    def __init__(self, in_folder, pdb_ids = None):
         self.dataset = []
-        for id in pdb_ids:
-            folder = id[1:3]
-            with open('{}/{}.features'.format(folder, id), 'rb') as fp:
-                data = pickle.load(fp)
-                self.dataset.append(data)
+        if pdb_ids:
+            for id in pdb_ids:
+                folder = id[1:3]
+                with open('{}/{}/{}.features'.format(in_folder, folder, id), 'rb') as fp:
+                    data = pickle.load(fp)
+                    self.dataset.append(data)
+        else:
+            for filename in glob.glob('{}/*/*.features'.format(in_folder)):
+                with open(filename, 'rb') as fp:
+                    data = pickle.load(fp)
+                    self.dataset.append(data)
 
     def dumpDataset(self, name='dataset'):
         with open(name + '.features', 'wb') as fp:
             pickle.dump(self.dataset, fp)
         return self.dataset
 
-class TERMDataLoader():
-    def __init__(self, filename, batch_size=4, shuffle=True,
-                 collate_fn=lambda x:x, drop_last=False):
-        with open(filename, 'rb') as fp:
-            dataset = pickle.load(fp)
+    def __len__(self):
+        return len(self.dataset)
 
+    def __getitem__(self, idx):
+        return self.dataset[idx]
+
+class TERMDataLoader():
+    def __init__(self, dataset, batch_size=4, shuffle=True,
+                 collate_fn=lambda x:x, drop_last=False):
         self.dataset = dataset
         self.size = len(dataset)
         self.lengths = [len(dataset[i]['focuses']) for i in range(self.size)]
@@ -54,18 +64,23 @@ class TERMDataLoader():
             batch = [self.dataset[i] for i in b_idx]
             focus_lens = [self.lengths[i] for i in b_idx]
             features, msas, focuses, seq_lens, src_masks = [], [], [], [], []
+            selfEs = []
             for data in batch:
                 # have to transpose these two because then we can use pad_sequence for padding
                 features.append(convert(data['features']).transpose(0,1))
                 msas.append(convert(data['msas']).transpose(0,1))
+                selfEs.append(convert(data['selfE']).transpose(0,1))
 
                 focuses.append(convert(data['focuses']))
                 seq_lens.append(data['seq_len'])
                 src_masks.append(data['mask'])
 
+
             # transpose back after padding
-            features = pad_sequence(features, batch_first=True).transpose(1,2).float()
+            features = pad_sequence(features, batch_first=True).transpose(1,2).double()
             msas = pad_sequence(msas, batch_first=True).transpose(1,2).long()
+            selfEs = pad_sequence(selfEs, batch_first=True).transpose(1,2)
+
             focuses = pad_sequence(focuses, batch_first=True)
             src_key_mask = pad_sequence([torch.zeros(l) for l in focus_lens], batch_first=True, padding_value=1).bool()
 
@@ -80,12 +95,12 @@ class TERMDataLoader():
             # invert at this stage, since masks are stored inverted
             padded_src_masks = ~(torch.stack(padded_src_masks).bool())
 
-            self.data_clusters.append([msas, features, seq_lens, focuses, padded_src_masks, src_key_mask])
+            self.data_clusters.append([msas, features, seq_lens, focuses, padded_src_masks, src_key_mask, selfEs])
 
     def __len__(self):
         return len(self.data_clusters)
 
     def __iter__(self):
-        np.random.shuffle(self.data_clusters)
+        #np.random.shuffle(self.data_clusters)
         for batch in self.data_clusters:
             yield batch
--- train_debug.py
+++ train_debug.py
@@ -2,19 +2,38 @@ from nets import *
 from data import *
 import pickle
 import torch
+import torch.optim as optim
+import torch.nn as nn
 import numpy as np
 import time
 
-dataloader = TERMDataLoader('temp.features', batch_size=2)
-condense = CondenseMSA(hidden_dim = 32, num_blocks=2)
+dev = 'cuda:0'
+dataset = Dataset('../MST_workspace/features')
+dataloader = TERMDataLoader(dataset, batch_size=1)
+condense = CondenseMSA(hidden_dim = 32, num_blocks=2, device = dev).double()
+reshape = DummyLSTM().to(dev).double()
 #print(condense)
-condense.cuda()
+condense.to(dev)
 
-for data in dataloader
-    msas, features, seq_lens, focus, src_mask, src_key_mask = data
-    msas = msas.cuda().long()
-    features = features.cuda().float()
-    focus = focus.cuda()
-    src_mask = src_mask.cuda()
-    src_key_mask = src_key_mask.cuda()
+criterion = nn.MSELoss()
+# dummy optimizer, replace with atom later
+optimizer = optim.SGD(condense.parameters(), lr=0.001, momentum=0.9)
+
+for i, data in enumerate(dataloader):
+    print('datapoint', i)
+    msas, features, seq_lens, focus, src_mask, src_key_mask, selfEs = data
+    msas = msas.to(dev)
+    features = features.to(dev)
+    focus = focus.to(dev)
+    src_mask = src_mask.to(dev)
+    src_key_mask = src_key_mask.to(dev)
+    selfEs = selfEs.to(dev)
     output = condense(msas, features, seq_lens, focus, src_mask, src_key_mask)
+    output = reshape(output)
+
+    optimizer.zero_grad()
+
+    loss = criterion(output, selfEs)
+    print(loss.item())
+    loss.backward()
+    optimizer.step()
