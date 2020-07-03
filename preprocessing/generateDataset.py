d2906d7b7545eccface34c1f367d7a61c7512130
alexjli <alexjli@mit.edu>
2020-07-03 11:11:27 -0500

    include etabs in data generation/loading

--- preprocessing/generateDataset.py
+++ preprocessing/generateDataset.py
@@ -4,9 +4,13 @@ import json
 import pickle
 import argparse
 from scipy.linalg import block_diag
+import glob
+import multiprocessing
+from joblib import Parallel, delayed
+
 from parseTERM import parseTERMdata
 from parseEtab import parseEtab
-import glob
+from mmtf_util import *
 
 
 def dumpTrainingTensors(in_path, out_path = None, cutoff = 1000, save=True):
@@ -69,18 +73,19 @@ def dumpTrainingTensors(in_path, out_path = None, cutoff = 1000, save=True):
     # check that sum of term lens is as long as the feature tensor
     assert sum(len_tensor) == features_tensor.shape[1]
 
-    # create attn mask for transformer
-    blocks = [np.ones((i,i)) for i in term_lens]
-    # create a block diagonal matrix mask
-    src_mask = block_diag(*blocks)
-    # note: masks need to be inverted upon use.
-    # they're stored this way so padding is easier later
+    pdb = in_path.split('/')[-1]
+    chain_dict = mmtf_parse(pdb, chain = 'A')
+
+    # Convert raw coords to np arrays
+    for key, val in chain_dict['coords'].items():
+        chain_dict['coords'][key] = np.asarray(val)
 
     output = {
+        'pdb': pdb,
+        'chain_dict': chain_dict,
         'features': features_tensor,
         'msas': msa_tensor,
         'focuses': term_focuses,
-        'mask': src_mask,
         'term_lens': len_tensor,
         'sequence': data['sequence'],
         'seq_len': len(data['selection']),
@@ -93,8 +98,7 @@ def dumpTrainingTensors(in_path, out_path = None, cutoff = 1000, save=True):
         if not out_path:
             out_path = ''
 
-        name = in_path.strip().split('/')[-1]
-        with open(out_path + name[:-4] + '.features', 'wb') as fp:
+        with open(out_path + '.features', 'wb') as fp:
             pickle.dump(output, fp)
 
     return output
@@ -124,16 +128,53 @@ def generateDataset(in_folder, out_folder, cutoff = 1000):
                 os.mkdir(full_folder_path)
             out_file = os.path.join(out_folder, name)
             print('out file', out_file)
-            output = dumpTrainingTensors(name, out_path = out_file, cutoff = cutoff)
-            dataset.append(output)
+            dumpTrainingTensors(name, out_path = out_file, cutoff = cutoff)
+            #output = dumpTrainingTensors(name, out_path = out_file, cutoff = cutoff)
+            #dataset.append(output)
 
-
+    """
     os.chdir(out_folder)
     # place the full dataset in the out_folder
     with open('full.dataset', 'wb') as fp:
         pickle.dump(dataset, fp)
+    """
 
     return dataset
 
+# https://medium.com/@mjschillawski/quick-and-easy-parallelization-in-python-32cb9027e490
+def generateDatasetParallel(in_folder, out_folder, cutoff = 1000):
+    # inner loop we wanna parallize
+    def dataGen(file, folder, out_folder, cutoff):
+        name = os.path.splitext(file)[0]
+        full_folder_path = os.path.join(out_folder, folder)
+        if not os.path.exists(full_folder_path):
+            os.mkdir(full_folder_path)
+        out_file = os.path.join(out_folder, name)
+        print('out file', out_file)
+        dumpTrainingTensors(name, out_path = out_file, cutoff = cutoff)
+
+    num_cores = multiprocessing.cpu_count()
+    # make folder where the dataset files are gonna be placed
+    if not os.path.exists(out_folder):
+        os.mkdir(out_folder)
+
+    # generate absolute paths so i dont have to think about relative references
+    in_folder = os.path.abspath(in_folder)
+    out_folder = os.path.abspath(out_folder)
+
+    os.chdir(in_folder)
+    # process folder by folder
+    for folder in glob.glob("*"):
+        # folders that aren't directories aren't folders!
+        if not os.path.isdir(folder):
+            continue
+
+        # for every file in the folder
+        Parallel(n_jobs=num_cores)(
+            delayed(dataGen)(file, folder, out_folder, cutoff) for file in glob.glob(folder + '/*.dat')
+        )
+
+
+
 if __name__ == '__main__':
-    generateDataset('dTERMen_data', 'features', cutoff = 50)
+    generateDatasetParallel('dTERMen_data', 'features', cutoff = 50)
