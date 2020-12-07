8549ec35181719799cb4826606a944ba26c67a7b
Vikram Sundar <vsundar@mit.edu>
2020-12-07 09:43:26 -0800

    Fix memory requirement for to_etab.

--- to_etab.sh
+++ to_etab.sh
@@ -3,6 +3,7 @@
 #SBATCH --partition=sched_mem1TB_centos7
 #SBATCH --time=06:00:00
 #SBATCH -n 16
+#SBATCH --mem=0
 #SBATCH -o logfiles/etab-output-OUTPUTDIR.out
 #SBATCH -e logfiles/etab-error-OUTPUTDIR.out
 
