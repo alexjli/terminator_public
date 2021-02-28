12b016c79bfa54fd68cccd8ff8f48b1e59b40ccc
alexjli <alexjli@mit.edu>
2021-02-28 17:23:43 -0500

    minor parameter updates and such

--- postprocessing/compress_files.sh
+++ postprocessing/compress_files.sh
@@ -2,7 +2,7 @@
 #SBATCH -N 1
 #SBATCH --partition=sched_mem1TB_centos7
 #SBATCH --time=06:00:00
-#SBATCH -n 1
+#SBATCH -n 16
 #SBATCH --mem=0
 #SBATCH -o logfiles/compress-files-output-OUTPUTDIR.out
 #SBATCH -e logfiles/compress-files-error-OUTPUTDIR.out
--- postprocessing/test_dTERMen_mass.py
+++ postprocessing/test_dTERMen_mass.py
@@ -4,6 +4,8 @@ import glob
 
 from filepaths import *
 
+#PDB_PATH='/scratch/users/alexjli/TERMinator/ingraham_db/PDB/'
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Run dTERMen for testing.')
     parser.add_argument('--output_dir', help = 'Output directory', default = 'test_run')
@@ -12,8 +14,9 @@ if __name__ == '__main__':
     output_path = os.path.join(OUTPUT_DIR, args.output_dir, 'etabs')
 
     for filename in glob.glob(os.path.join(output_path, '*.etab')):
-        pdb_id = filename[-9:-5]
+        pdb_id = filename[-9:-5] #filename[-11:-5] # 
         print(pdb_id)
         os.system(f"cp {PDB_PATH}{pdb_id.lower()[1:3]}/{pdb_id}.pdb {output_path}/{pdb_id}.pdb")
         os.system(f"sed -e \"s/ID/{pdb_id}/g\" -e 's/OUTPUTDIR/{args.output_dir}/g' </home/alexjli/TERMinator/postprocessing/run_dTERMen.sh >{output_path}/run_{pdb_id}.sh")
         os.system(f"cd {output_path} && sbatch run_{pdb_id}.sh")
+
