f569a8d0baf5e5ce2e10cc14b14f97278007b399
alexjli <alexjli@mit.edu>
2021-03-20 22:31:30 -0400

    path reference fix

--- postprocessing/fix_dTERMen.py
+++ postprocessing/fix_dTERMen.py
@@ -13,5 +13,5 @@ if __name__ == '__main__':
             print(pdb_id)
             os.system(f'cp -r {filepath} /scratch/users/alexjli/TERMinator/fixed_dTERMen/')
             os.system(f'cp {PDB_PATH}{pdb_id.lower()[1:3]}/{pdb_id}.pdb /scratch/users/alexjli/TERMinator/fixed_dTERMen/{pdb_id}/{pdb_id}.pdb')
-            os.system(f"sed -e \"s/ID/{pdb_id}/g\" </home/alexjli/TERMinator/fix_dTERMen.sh >/scratch/users/alexjli/TERMinator/fixed_dTERMen/{pdb_id}/fix_{pdb_id}.sh")
+            os.system(f"sed -e \"s/ID/{pdb_id}/g\" </home/alexjli/TERMinator/postprocessing/fix_dTERMen.sh >/scratch/users/alexjli/TERMinator/fixed_dTERMen/{pdb_id}/fix_{pdb_id}.sh")
             os.system(f"cd /scratch/users/alexjli/TERMinator/fixed_dTERMen/{pdb_id}/ && sbatch fix_{pdb_id}.sh")
