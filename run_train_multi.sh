c6a07adf37debe7bdecb2c9037fc647f71f22ab8
alexjli <alexjli@mit.edu>
2021-03-20 22:25:26 -0400

    slight sbatch config changes

--- run_train_multi.sh
+++ run_train_multi.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH -n 16
-#SBATCH --mem=256GB
-#SBATCH --gres=gpu:2
-#SBATCH --partition=sched_system_all
-#SBATCH --time=12:00:00
+#SBATCH --gres=gpu:volta:2
+#SBATCH --time=18:00:00
+#SBATCH --mem=128G
 #SBATCH -o logfiles/test-output_RUNNAME_foldFOLD_runRUNNO.out
 #SBATCH -e logfiles/test-error_RUNNAME_foldFOLD_runRUNNO.out
 
 HOME2=/nobackup/users/alexjli
 CONDA_ROOT=$HOME2/anaconda3
 source ${CONDA_ROOT}/etc/profile.d/conda.sh
-conda activate pytorch1.5
+conda activate pytorch1.7
 ulimit -s unlimited
 
 python train_multi.py --dataset=features_singlechain --train=fold_splits/train_foldFOLD.in --val=fold_splits/val_foldFOLD.in --test=fold_splits/test_foldFOLD.in --run_name=test_run_foldFOLD_RUNNAME --hparams=hparams/HPARAMS.json
