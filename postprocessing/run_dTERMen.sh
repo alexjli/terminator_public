3ac89cdb71ac896118aa999aab9a487047cc43cf
alexjli <alexjli@mit.edu>
2021-05-19 18:54:22 -0400

    change executable

--- postprocessing/run_dTERMen.sh
+++ postprocessing/run_dTERMen.sh
@@ -7,4 +7,4 @@
 #SBATCH -e /scratch/users/alexjli/TERMinator_runs/OUTPUTDIR/etabs/ID-error.err
 
 . /etc/profile.d/modules.sh
-/scratch/users/swans/MST_workspace/MST/bin/design --p ID.pdb --o ID --c /scratch/users/vsundar/TERMinator/default_configfile
+~/MST_workspace/MST/bin/design --p ID.pdb --o ID --c /scratch/users/vsundar/TERMinator/default_configfile
