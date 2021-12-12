bd0ce3031bb1a5dfb9b09b821c531bf4c1b83ab5
Vikram Sundar <vsundar@mit.edu>
2021-12-12 18:14:24 -0500

    Eliminate versionneer and fix init files.

--- terminator/__init__.py
+++ terminator/__init__.py
@@ -1,12 +1,1 @@
 """ Neuronal version of dTERMen """
-
-# Imports
-from . import *
-
-# Handle versioneer
-from ._version import get_versions
-
-versions = get_versions()
-__version__ = versions['version']
-__git_revision__ = versions['full-revisionid']
-del get_versions, versions
